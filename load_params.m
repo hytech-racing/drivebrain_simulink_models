a29f7a9d6daebf47c334e5b38587947367ed0dc9
Ben Hall <rcmast3r1@gmail.com>
2025-05-29 22:14:08 -0400

    fixing load script

--- load_params.m
+++ load_params.m
@@ -15,10 +15,5 @@ for k = 1:length(matFiles)
     % Display which file is being loaded
     fprintf('Loading %s\n', filePath);
 
-    % Load the contents into a struct to avoid overwriting variables
-    dataStruct = load(filePath);
-
-    % Optionally assign to a variable with the same name as the file
-    [~, fileBaseName, ~] = fileparts(matFiles(k).name);
-    assignin('base', fileBaseName, dataStruct);
+    load(filePath);
 end
