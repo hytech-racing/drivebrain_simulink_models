e84dea1323a9a6f1658b6604d94ff073103061b4
Ben Hall <rcmast3r1@gmail.com>
2025-07-09 20:58:19 -0400

    yup dont care

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rm -rf slprj
-rm */*/*.autosave
-rm */*.autosave
-rm -rf *_ert_rtw
-rm *.zip
-rm *.slxc
+rm -rf slprj || true
+rm */*/*.autosave || true
+rm */*.autosave || true
+rm -rf *_ert_rtw || true
+rm *.zip || true
+rm *.slxc || true
